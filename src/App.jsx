727787eb139da2ec142a36611268c7ec95cfec41
מוני ברוך <monybaruch@gmail.com>
2023-09-04 03:27:10 +0300

    refctoring the page links to be dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Services from './components/Services';
 import Tour from './components/Tour';
+import Hero from './components/Hero';
 
 const url = 'https://course-api.com/react-tours-project';
 
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const pageLinks = [
+  {
+    id: 1,
+    href: '#home',
+    text: 'home',
+  },
+  {
+    id: 1,
+    href: '#about',
+    text: 'about',
+  },
+  {
+    id: 1,
+    href: '#services',
+    text: 'services',
+  },
+  {
+    id: 1,
+    href: '#tours',
+    text: 'tours',
+  },
+];
